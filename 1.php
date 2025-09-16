b06d5fa2b61b244dec2f3ecd2768d1f8da2d2b83
School User <prostoakk0912@gmail.com>
2025-09-16 16:42:39 +0200

    Initial commit

---
+++ 1.php
@@ -0,0 +1,7 @@
+<?php 
+    echo("Hello world from PHP! <br>");
+    echo("<h3 style="color:blue;">Hello world from PHP</h3>");
+    printf("Hello world from %s", "PHP<br>");
+    print "Hello world from PHP<br>"
+    printf("<h1>%s</h1>; Hello world from PHP");
+?>
