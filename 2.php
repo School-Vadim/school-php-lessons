dc9c1b86d817506c94693499743f570ce76b0cfe
School user <prostoakk0912@gmail.com>
2025-09-17 12:39:17 +0200

    added 2.php

---
+++ 2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TEST</title>
+</head>
+<body text="green">
+    <h1 style="color: red;">Hello world from PHP</h1>
+    <?php
+        echo("Hello world from PHP! <br>");
+        echo("<h3 style=color:blue;>Hello world from PHP</h3>");
+        printf("Hello world from %s", "PHP<br>");
+        print "Hello world from PHP<br>";
+        printf("<h1>%s</h1>", "Hello world from PHP");
+    ?>
+</body>
+</html>
