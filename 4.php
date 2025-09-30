d10741951a2bc1141f20160064db04df7270c621
School-User <prostoakk0912@gmail.com>
2025-09-30 16:37:49 +0200

    4.php

---
+++ 4.php
@@ -0,0 +1,12 @@
+<?php
+    $num = 8;
+
+    if ($num > 10) {
+        echo("Number $num is bigger then 10");
+    } else if($num == 10) {
+        echo("Number $num is equal 10");
+    } else {
+        echo("Number $num is less then 10");
+    }
+
+?>
