3ba9eee3ab14a9cd2aadd8d5a92a93b37a4bde00
School-User <prostoakk0912@gmail.com>
2025-10-28 16:31:58 +0100

    fix

--- 8.php
+++ 8.php
@@ -3,7 +3,7 @@
     $i = 1;
     do {
         $result = $i * $i;
-        printf("%d * %d = %d", $i, $i, $result);
+        printf("%d * %d = %d<br>", $i, $i, $result);
         $i++;
     } while($i < 10);
 
